a3a5aafa417112fbe3839f71edc742e2f09a0b63
Anatoliy Sablin <tolya@sablin.xyz>
2020-08-26 01:16:56 +0300

    Initial commit of the jru backend.

---
+++ backend/models/__init__.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from backend.models.article import Article
+from backend.models.articles import Articles
+from backend.models.articles_response import ArticlesResponse
+from backend.models.auth import Auth
+from backend.models.domain_list import DomainList
+from backend.models.domains import Domains
+from backend.models.error_result import ErrorResult
+from backend.models.feedback import Feedback
+from backend.models.mam_message import MAMMessage
+from backend.models.mam_message_list import MAMMessageList
+from backend.models.mam_message_list_response import MAMMessageListResponse
+from backend.models.mam_message_request import MAMMessageRequest
+from backend.models.mam_settings import MAMSettings
+from backend.models.mam_settings_response import MAMSettingsResponse
+from backend.models.one_of_register import OneOfRegister
+from backend.models.register import Register
+from backend.models.register_email import RegisterEmail
+from backend.models.register_password import RegisterPassword
+from backend.models.roster import Roster
+from backend.models.roster_list import RosterList
+from backend.models.roster_list_response import RosterListResponse
+from backend.models.v_card import VCard
+from backend.models.v_card_response import VCardResponse
